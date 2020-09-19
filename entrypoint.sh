4c281afedd3796be70e1fe511adaa59ba6d0a1dd
Luís Guimarães <35278323+obvionaoe@users.noreply.github.com>
2020-09-19 17:17:02 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z $DB_CONNECTION_GHZ  ]; then
+  case $GHZ_DATABASE_TYPE in
+    mysql )
+      export DB_CONNECTION_GHZ=$GHZ_DB_USER:$GHZ_DB_PASSWORD@/ghz
+    ;;
+    postgres )
+      export DB_CONNECTION_GHZ="host=$GHZ_DB_HOST user=$GHZ_DB_USER dbname=$GHZ_DB_NAME sslmode=$GHZ_DB_SSL_MODE password=$GHZ_DB_PASSWORD"
+    ;;
+  esac
+fi
