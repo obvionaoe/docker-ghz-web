696213bb396d1d5f4e94e2ec957082834c85b323
Luís Guimarães <35278323+obvionaoe@users.noreply.github.com>
2020-09-19 17:21:39 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -z $DB_CONNECTION_GHZ  ]; then
+if [ -z $GHZ_DATABASE_CONNECTION  ]; then
   case $GHZ_DATABASE_TYPE in
     mysql )
-      export DB_CONNECTION_GHZ=$GHZ_DB_USER:$GHZ_DB_PASSWORD@/ghz
+      export GHZ_DATABASE_CONNECTION=$GHZ_DB_USER:$GHZ_DB_PASSWORD@/ghz
     ;;
     postgres )
-      export DB_CONNECTION_GHZ="host=$GHZ_DB_HOST user=$GHZ_DB_USER dbname=$GHZ_DB_NAME sslmode=$GHZ_DB_SSL_MODE password=$GHZ_DB_PASSWORD"
+      export GHZ_DATABASE_CONNECTION="host=$GHZ_DB_HOST user=$GHZ_DB_USER dbname=$GHZ_DB_NAME sslmode=$GHZ_DB_SSL_MODE password=$GHZ_DB_PASSWORD"
     ;;
   esac
 fi
